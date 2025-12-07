878c2cc937624fd75692874e0791c03084adb6c9
Dang Tran <114612920+dangtrxn@users.noreply.github.com>
2025-12-07 15:44:23 -0500

    Delete domain/node/ExpressionNode.java

--- domain/node/ExpressionNode.java
+++
@@ -1,7 +0,0 @@
-package domain.node;
-/**
- * abstract class for an expression, contains abstract evaluate method
- */
-public abstract class ExpressionNode {
-    public abstract int evaluate();
-}
